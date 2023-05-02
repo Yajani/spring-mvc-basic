4e0c72bf3200935a62f27cd32c1d578b810d76f5
yajani <115179558+Yajani@users.noreply.github.com>
2023-05-02 17:33:50 +0900

    # 11-1. 댓글 SQL 처리 및 테스트

--- src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
+++ src/test/java/com/spring/mvc/chap05/repository/ReplyMapperTest.java
@@ -1,7 +1,5 @@
 package com.spring.mvc.chap05.repository;
 
-import com.spring.mvc.chap05.dto.page.Search;
-import com.spring.mvc.chap05.entity.Board;
 import com.spring.mvc.chap05.entity.Reply;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ReplyMapperTest {
@@ -20,31 +19,31 @@ class ReplyMapperTest {
     @Autowired
     ReplyMapper replyMapper;
 
-    @Test
-    @DisplayName("게시물 300개를 등록하고 " +
-            "각 게시물에 랜덤으로 1000개의 " +
-            "댓글을 나눠서 등록해야 한다.")
-    void bulkInsertTest() {
-
-        for (int i = 1; i <= 300; i++) {
-            Board b = Board.builder()
-                    .title("재밌는 게시물 " + i)
-                    .content("노잼 게시물 내용 " + i)
-                    .build();
-            boardMapper.save(b);
-        }
-        assertEquals(300, boardMapper.count(new Search()));
-
-        for (int i = 1; i <= 1000; i++) {
-            Reply r = Reply.builder()
-                    .replyWriter("잼민이 " + i)
-                    .replyText("말똥이~~~" + i)
-                    .boardNo((long) (Math.random() * 300 + 1))
-                    .build();
-            replyMapper.save(r);
-        }
-
-    }
+//    @Test
+//    @DisplayName("게시물 300개를 등록하고 " +
+//            "각 게시물에 랜덤으로 1000개의 " +
+//            "댓글을 나눠서 등록해야 한다.")
+//    void bulkInsertTest() {
+//
+//        for (int i = 1; i <= 300; i++) {
+//            Board b = Board.builder()
+//                    .title("재밌는 게시물 " + i)
+//                    .content("노잼 게시물 내용 " + i)
+//                    .build();
+//            boardMapper.save(b);
+//        }
+//        assertEquals(300, boardMapper.count(new Search()));
+//
+//        for (int i = 1; i <= 1000; i++) {
+//            Reply r = Reply.builder()
+//                    .replyWriter("잼민이 " + i)
+//                    .replyText("말똥이~~~" + i)
+//                    .boardNo((long) (Math.random() * 300 + 1))
+//                    .build();
+//            replyMapper.save(r);
+//        }
+//
+//    }
 
 
     @Test
