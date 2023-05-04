08c8e5fa45de6bc2cd56cb43e499af1a06aaf89a
yajani <115179558+Yajani@users.noreply.github.com>
2023-05-04 11:55:05 +0900

    # 11-5 댓글 삭제처리

--- src/main/java/com/spring/mvc/chap05/api/ReplyController.java
+++ src/main/java/com/spring/mvc/chap05/api/ReplyController.java
@@ -1,15 +1,15 @@
 package com.spring.mvc.chap05.api;
 
 import com.spring.mvc.chap05.dto.ReplyListResponseDTO;
+import com.spring.mvc.chap05.dto.ReplyPostRequestDTO;
 import com.spring.mvc.chap05.dto.page.Page;
 import com.spring.mvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -41,5 +41,63 @@ public class ReplyController {
                 = replyService.getList(boardNo, page);
 
         return ok().body(replyList);
+    }
+
+    //댓글 등록 요청
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<?> create(
+            // 요청 메시지 바디에 JSON으로 보내주세요
+            @Validated @RequestBody ReplyPostRequestDTO dto
+            , BindingResult result //검증결과를 가진 객체
+    ) {
+        // 입력값 검증에 걸리면 4xx 상태코드 리턴
+        if (result.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.toString());
+        }
+        log.info("/api/v1/replies : POST ! ");
+        log.info("param: {} ", dto);
+
+        //서비스에 비즈지스 로직 처리 위임
+        try {
+            ReplyListResponseDTO responseDTO = replyService.register(dto);
+            //성공시 클라이언트에 응답하기
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            // 문제발생 상황을 클라이언트에게 전달
+            log.warn("5000 Status code response !! caused by: {}", e.getMessage());
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage()); //서비스에 있는 "댓글 저장실패"
+        }
+    }
+
+    // 댓글 삭제 요청
+    @DeleteMapping("/{replyNo}")
+    public ResponseEntity<?> remove(
+            @PathVariable(required = false) Long replyNo
+    ) {
+        if (replyNo == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("댓글 번호를 보내주세요!");
+        }
+
+        log.info("/api/v1/replies/{} DELETE!", replyNo);
+
+        try {
+            ReplyListResponseDTO responseDTO
+                    = replyService.delete(replyNo);
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage());
+        }
+
     }
 }
